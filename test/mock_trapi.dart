8a189dc461d1100194f227325f4aad14512d69b6
USUARIO-PC\USUARIO <jrinconada@gmail.com>
2020-08-10 20:07:45 +0200

    Improve Mock API tests

--- test/mock_trapi.dart
+++ test/mock_trapi.dart
@@ -1,4 +1,3 @@
-
 import 'package:dartz/dartz.dart';
 import 'package:trappers/models/failure.dart';
 import 'package:trappers/models/trapper.dart';
@@ -6,13 +5,28 @@ import 'package:trappers/services/trapi.dart';
 
 /// Manages network connections with the API
 class MockTrapi implements Trapi {
+  bool error = false;
+  bool delay = false;
 
-  List<Trapper> prepareTrappers () {
+  List<Trapper> prepareTrappers() {
     List<Trapper> trappers = new List();
-    var info = {    
+    var info = {
       "name": "Rosalia",
       "genre": "Trap flamenco",
-      "pic": "https://i.avoz.es/sc/rMsptQU3g42HxOaAATfHlHatfdU=/x/2018/11/07/00121541584284025196450/Foto/rosalia.jpg"    
+      "pic":
+          "https://i.avoz.es/sc/rMsptQU3g42HxOaAATfHlHatfdU=/x/2018/11/07/00121541584284025196450/Foto/rosalia.jpg"
+    };
+    trappers.add(Trapper.fromJson(info));
+    info = {
+        "name": "Post Malone",
+        "genre": "Pop Trap",
+        "pic": "https://e00-elmundo.uecdn.es/assets/multimedia/imagenes/2018/07/11/15313304669358.jpg"
+    };
+    trappers.add(Trapper.fromJson(info));
+    info = {
+        "name": "Kinder Malo",
+        "genre": "Trap irónico",
+        "pic": "https://i.ytimg.com/vi/j8SbnkUhgnc/maxresdefault.jpg"
     };
     trappers.add(Trapper.fromJson(info));
     return trappers;
@@ -20,8 +34,10 @@ class MockTrapi implements Trapi {
 
   @override
   Future<Either<Failure, List<Trapper>>> getTrappers() async {
-    await Future.delayed(const Duration(seconds: 3)); // Take some time
-    return Right(prepareTrappers()); // Ok
-    return Left(Failure('Network error')); // Error
+    if (delay) await Future.delayed(const Duration(seconds: 3)); // Take some time
+    if (!error)
+      return Right(prepareTrappers()); // Ok
+    else
+      return Left(Failure('Network error')); // Error
   }
 }
--- test/trapper_list_test.dart
+++ test/trapper_list_test.dart
@@ -26,16 +26,41 @@ Widget makeTestable({Widget child, Trapi trapi}) {
 }
 
 void main() {
-  testWidgets('All trappers information is displayed in a list',
-      (WidgetTester tester) async {
-    // Prepare fake data
-    Trapi trapi = MockTrapi();
+  group('Trapper List', () {
+    MockTrapi trapi;
+    setUpAll(() {
+      trapi = MockTrapi();
+    });
 
-    // Build the screen and call API
-    await tester.pumpWidget(makeTestable(child: TrapperList(), trapi: trapi));
-    await tester.pumpAndSettle(); // Wait for it load    
+    testWidgets('All trappers information is displayed in a list',
+        (WidgetTester tester) async {
+      // Prepare fake data
+      trapi.delay = true;
+      List<Trapper> trappers = trapi.prepareTrappers();
 
-    // Find information on the screen
-    expect(find.text('Rosalia'), findsOneWidget);
+      // Build the screen and call API
+      await tester.pumpWidget(makeTestable(child: TrapperList(), trapi: trapi));
+      await tester.pumpAndSettle(); // Wait for it load
+
+      // Find information on the screen
+      for (var trapper in trappers) {
+        expect(find.text(trapper.name), findsOneWidget);
+        expect(find.text(trapper.genre), findsOneWidget);
+      }
+    });
+
+    testWidgets('On network failure an error is displayed',
+        (WidgetTester tester) async {
+      // Prepare fake API      
+      trapi.delay = true;
+      trapi.error = true;
+
+      // Build the screen and call API
+      await tester.pumpWidget(makeTestable(child: TrapperList(), trapi: trapi));
+      await tester.pumpAndSettle(); // Wait for it load
+
+      // Find information on the screen
+      expect(find.text('Network error'), findsOneWidget);
+    });
   });
 }
